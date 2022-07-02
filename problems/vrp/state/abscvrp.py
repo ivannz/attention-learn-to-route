c4f7d02d6bb4b254b99cbead2161518921210186
Ivan Nazarov <ivannz@yandex.ru>
2022-07-02 11:16:43 +0300

    improve proposal, clean up beam search, validate implementation

--- problems/vrp/state/abscvrp.py
+++ problems/vrp/state/abscvrp.py
@@ -1,3 +1,4 @@
+# import math
 import torch
 
 from typing import NamedTuple
@@ -12,8 +13,8 @@ class AbsCVRP(NamedTuple):
     partial: bool
     # [B x N x N] large dense shortest path matrix used as reference
     distances: Tensor
-    # [S] the instance at each beam S -->> B
-    beam: Tensor
+    # [S] index to instance correspondence S -->> B surjective
+    instance: Tensor
     # [S x ...] the runtime state
     # [S x N] residual demand
     demand: Tensor
@@ -25,8 +26,8 @@ class AbsCVRP(NamedTuple):
     capacity: Tensor
     # [S] flag indicating if the simulation has terminated
     done: Tensor
-    # [S] the cumulative costs
-    costs: Tensor
+    # [S] the cumulative route cost
+    cost: Tensor
 
     # class-level constant
     MAX_CAPACITY = 1.0
@@ -45,19 +46,21 @@ class AbsCVRP(NamedTuple):
         return cls(
             partial=input["partial"],
             distances=dis,
-            beam=torch.arange(len(dis), device=dis.device),
+            instance=torch.arange(len(dis), device=dis.device),
             demand=dem,
             mask=dem.le(0),
             capacity=dis.new_zeros(len(dis)).fill_(cls.MAX_CAPACITY),
             done=dis.new_zeros(len(dis), dtype=bool),
             loc=dis.new_zeros(len(dis), dtype=torch.long),
-            costs=dis.new_zeros(len(dis)),
+            cost=dis.new_zeros(len(dis)),
         )
 
     def update(self, selected):
+        assert len(self.instance) == len(selected)
+
         # update costs with links from the current location and to endpoints
         # d_{b v_b \to w_b} for `loc` v_b and `selected` w_b
-        costs = self.costs.add(self.distances[self.beam, self.loc, selected])
+        cost = self.cost.add(self.distances[self.instance, self.loc, selected])
 
         # we deliver to the `n`-th client as much as we can when visiting them
         picked = selected.unsqueeze(-1)
@@ -65,9 +68,9 @@ class AbsCVRP(NamedTuple):
 
         # the change in capacity: +ve we refilled, -ve we delivered
         delta = demand_at.clamp(
-            min=self.capacity - self.MAX_CAPACITY, max=self.capacity
-        )
-        delta.neg_()
+            min=self.capacity - self.MAX_CAPACITY,
+            max=self.capacity,
+        ).neg_()
 
         # compute new capacity and demands (produce copies)
         capacity = self.capacity.add(delta)
@@ -96,19 +99,18 @@ class AbsCVRP(NamedTuple):
             loc=selected.clone(),
             capacity=capacity,
             done=done,
-            costs=costs,
+            cost=cost,
         )
 
     def __getitem__(self, index):
-        assert isinstance(index, (Tensor, slice))
         return self._replace(
-            beam=self.beam[index],
+            instance=self.instance[index],
             demand=self.demand[index],
             mask=self.mask[index],
             loc=self.loc[index],
             capacity=self.capacity[index],
             done=self.done[index],
-            costs=self.costs[index],
+            cost=self.cost[index],
         )
 
     @property
@@ -121,7 +123,7 @@ class AbsCVRP(NamedTuple):
 
     def get_final_cost(self):
         assert self.done.all()
-        return self.costs
+        return self.cost
 
     def all_finished(self):
         return self.done.all()
@@ -155,6 +157,8 @@ def topk_indices(input, k, dim=-1, largest=True, sorted=True, *, groups=None):
     # get the start index of each group and do the splitting
     indptr = edges.nonzero()[:, 0]
     splits = input.split(indptr.diff().tolist(), dim)
+    if not splits:
+        return indptr.new_empty(0)
 
     # get the indices of top-k in-group values (recurrence)
     return torch.cat(
@@ -167,83 +171,91 @@ def topk_indices(input, k, dim=-1, largest=True, sorted=True, *, groups=None):
 
 
 class Beam(NamedTuple):
+    # `parent` indicates which beam the current beam came from
     parent: Tensor
-    state: None
-    score: Tensor
     action: Tensor
+    score: Tensor
+    state: None
 
     @classmethod
     def initialize(cls, state):
         return cls(
-            parent=state.beam.clone(),
-            state=state,
-            score=state.costs.new_zeros(state.beam.shape),
+            parent=torch.arange(len(state.cost), device=state.cost.device),
             action=None,
+            score=state.cost.new_zeros(len(state.cost)),
+            state=state,
         )
 
-    def expand(self, parent, action, score=None):
-        return self._replace(
-            parent=parent,
-            state=self.state[parent].update(action),
-            score=score,
-            action=action,
+    def expand(self, parent, action, score):
+        # populate new states from parents and update each with an action
+        state = self.state[parent].update(action)
+        return type(self)(parent, action, score, state)
+
+    def select(self, k, *, largest=True):
+        # select the top-k scoring beams within each bundle, formed be grouping
+        # beams by instance id of the associated problem in `.state`.
+        top_k = topk_indices(
+            self.score,
+            k,
+            largest=largest,
+            groups=self.state.instance,
+        )
+        return type(self)(
+            self.parent[top_k],
+            None if self.action is None else self.action[top_k],
+            self.score[top_k],
+            self.state[top_k],
         )
 
-    def select(self, k, largest=True):
-        idx = topk_indices(self.score, k, largest=largest, groups=self.state.beam)
-        return self._replace(
-            parent=None if self.parent is None else self.parent[idx],
-            state=self.state[idx],
-            score=self.score[idx],
-            action=None if self.action is None else self.action[idx],
-        )
 
-
-def beam_search(state, n_beam_k, largest=True, propose=None, keep_states=False):
+def beam_search(state, k, propose=None, *, commit=id, largest=True):
     beam = Beam.initialize(state)
+    commit(beam)
 
-    history = []
-    append = history.append if keep_states else id
-
-    append(beam)
+    # propose expansion for each beam, materialize them and then filter
     while not beam.state.done.all():
         parent, action, score = propose(beam)
-        if parent is None:
-            return history, None
+        if len(parent) < 1:
+            break
 
-        # Expand and update the state according to the selected actions
-        beam = beam.expand(parent, action, score=score).select(n_beam_k, largest)
-        append(beam)
+        beam = beam.expand(parent, action, score).select(k, largest=largest)
+        commit(beam)
 
-    # Return the final state separately since beams may not keep state
-    return history, beam.state
+    return beam
 
 
-def propose(beam, k=3):
-    raw = torch.randn_like(beam.state.demand).masked_fill_(
-        beam.state.mask, float("-inf")
-    )
-    logits = raw.log_softmax(-1)
+def propose(beam, *, k=3, kind):
+    """A dummy beam expansion proposal."""
+    assert kind in ("greedy", "dummy")
 
-    log_p, top_k = logits.topk(k, dim=-1)
+    if kind == "greedy":
+        raw = beam.state.distances[beam.state.instance, beam.state.loc].neg()
+        logits = raw.masked_fill_(beam.state.mask, float("-inf"))
 
-    # get expanded beam scores
-    parent = torch.arange(len(top_k)).repeat_interleave(k)
+    else:
+        raw = torch.randn_like(beam.state.demand)
+        raw.masked_fill_(beam.state.mask, float("-inf"))
+        logits = raw.log_softmax(-1)
+
+    log_p, top_k = logits.topk(k, dim=-1, largest=True)
+
+    parent = torch.arange(len(logits)).repeat_interleave(k)
+    scores = log_p.add_(beam.score.unsqueeze(-1)).flatten()
     action = top_k.flatten()
-    scores = torch.flatten(beam.score.unsqueeze(-1) + log_p)
 
     is_finite = scores.isfinite()
-
     return parent[is_finite], action[is_finite], scores[is_finite]
 
 
 if __name__ == "__main__":
+    from functools import partial
+
     # distances
     n_batch_size, n_loc = 64, 50
     e = torch.rand(n_batch_size, 1 + n_loc, 1 + n_loc).log_().neg_()
     e.diagonal(dim1=-2, dim2=-1)[:] = 0
-    for k in range(e.shape[1]):
-        torch.minimum(e, e[..., :, [k]] + e[..., [k], :], out=e)
+    for j in range(e.shape[1]):
+        torch.minimum(e, e[..., :, [j]] + e[..., [j], :], out=e)
 
     # demands
     x = torch.rand(len(e), 1 + n_loc)
@@ -254,7 +266,30 @@ if __name__ == "__main__":
     input = dict(distances=e, demand=x, partial=False)
 
     state = AbsCVRP.initialize(input)
-    history, out = beam_search(state, 5, propose=propose, keep_states=True)
+    history = []
+    out = beam_search(
+        state,
+        k=9,
+        propose=partial(propose, k=3, kind="dummy"),
+        commit=history.append,
+    )
+
+    # backtrack thru the saved beam history and rebuild the sequence
+    #  $\pi_{t:} = \pi_t \circ \pi_{t+1:}$
+    actions = []
+    beam = out.select(1, largest=True)
+    parent, score = beam.parent, beam.score
+    # parent, score = slice(None), history[-1].score
+    while history:
+        beam = history.pop()
+        if beam.action is None:
+            break
+
+        # get parents' actions and their parents
+        actions.append(beam.action[parent])
+        parent = beam.parent[parent]
+
+    actions = torch.stack(actions[::-1], dim=-1)
 
     self = AbsCVRP.initialize(input)
     actions = []
