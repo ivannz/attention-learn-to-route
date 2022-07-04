b86bc26111b110da9f970dbf4dda4c744ecb494e
Ivan Nazarov <ivannz@yandex.ru>
2022-07-04 21:03:37 +0300

    implement tour cost evaluation

--- problems/vrp/problem_vrp/cvrp.py
+++ problems/vrp/problem_vrp/cvrp.py
@@ -104,7 +104,6 @@ class AbsCVRP:
 
     @classmethod
     def get_costs(cls, dataset, pi):
-        raise RuntimeError
 
         batch_size, graph_size = dataset["demand"].size()
         # Check that tours are valid, i.e. contain 0 to n -1
@@ -112,49 +111,33 @@ class AbsCVRP:
 
         # Sorting it should give all zeros at front and then 1...n
         assert (
-            torch.arange(1, graph_size + 1, out=pi.data.new())
+            torch.arange(1, graph_size, out=pi.data.new())
             .view(1, -1)
             .expand(batch_size, graph_size)
             == sorted_pi[:, -graph_size:]
         ).all() and (sorted_pi[:, :-graph_size] == 0).all(), "Invalid tour"
 
-        # Visiting depot resets capacity so we add demand = -capacity (we make sure
-        #  it does not become negative)
-        demand_with_depot = torch.cat(
-            (
-                torch.full_like(dataset["demand"][:, :1], -CVRP.VEHICLE_CAPACITY),
-                dataset["demand"],
-            ),
-            1,
-        )
-        d = demand_with_depot.gather(1, pi)
+        # clamp supply at -capacity
+        dem = dataset["demand"].gather(1, pi)
+        dem[dem.isinf()] = -cls.VEHICLE_CAPACITY
 
         used_cap = torch.zeros_like(dataset["demand"][:, 0])
         for i in range(pi.size(1)):
-            used_cap += d[
-                :, i
-            ]  # This will reset/make capacity negative if i == 0, e.g. depot visited
+            used_cap += dem[:, i]
+            # This will reset/make capacity negative if i == 0, e.g. depot visited
             # Cannot use less than 0
             used_cap[used_cap < 0] = 0
             assert (
-                used_cap <= CVRP.VEHICLE_CAPACITY + 1e-5
+                used_cap <= cls.VEHICLE_CAPACITY + 1e-5
             ).all(), "Used more than capacity"
 
-        # Gather dataset in order of tour
-        loc_with_depot = torch.cat((dataset["depot"][:, None, :], dataset["loc"]), 1)
-        d = loc_with_depot.gather(
-            1, pi[..., None].expand(*pi.size(), loc_with_depot.size(-1))
-        )
+        # gather dataset in order of tour, and pad with depots
+        end = torch.full_like(pi[:, :1], 0)
+        pad = torch.cat((end, pi, end), dim=-1)
 
-        # Length is distance (L2-norm of difference) of each next location to its
-        #  prev and of first and last to depot
-        return (
-            (d[:, 1:] - d[:, :-1]).norm(p=2, dim=2).sum(1)
-            + (d[:, 0] - dataset["depot"]).norm(p=2, dim=1)  # Depot to first
-            + (d[:, -1] - dataset["depot"]).norm(
-                p=2, dim=1
-            )  # Last to depot, will be 0 if depot is last
-        ), None
+        dist = dataset["distances"]
+        idx = torch.arange(len(dist), device=dist.device).unsqueeze(-1)
+        return dist[idx, pad[:, :-1], pad[:, 1:]].sum(-1), None
 
     @staticmethod
     def make_state(*args, **kwargs):
